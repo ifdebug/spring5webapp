28eb5cad678a1a60b78155d9ae5f40ec43ddeb78
ifdebug <830552+ifdebug@users.noreply.github.com>
2025-08-21 01:42:40 -0600

    Pushing to work from shop

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,14 +1,31 @@
 package guru.springframework.spring5webapp.domain;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String firstName;
     private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     public Author() {
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
